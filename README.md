fc74fef98c402b3bd42ceb05a8550ad75ecdb774
Georges Delrieu <georges.delrieu@sciencespo.fr>
2022-01-06 18:08:14 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # planetly-takehome
+
+
+to get things started
+````
+docker run -d --name testcontainer -p 80:80 -v /var/run/docker.sock:/var/run/docker.sock myimage
+````
+
+
+docker on docker doc:
+https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
+
---
+++ app/main.py
@@ -0,0 +1,37 @@
+#FastAPI and Pydantic Related Libraries
+from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
+from typing import List
+
+#APScheduler Related Libraries
+from apscheduler.schedulers.background import BackgroundScheduler
+import docker
+import logging
+
+app = FastAPI(title="Status State API",description="Recurringly checks the status of locally running containers")
+logger = logging.getLogger(__name__)
+
+def listing_containers():
+    client = docker.from_env()
+    running_containers = client.containers.list()
+    return [{"name": str(running_container.name),
+            "status": str(running_container.status),
+            "image": str(running_container.image),
+            "container ID": str(running_container.id)} 
+            for running_container in running_containers]
+
+
+scheduler = BackgroundScheduler(daemon = True)
+scheduler.add_job(listing_containers, 'interval', seconds=30)
+
+@app.on_event("startup")
+def display_statuses():
+    scheduler.start()
+    logger.info("Monitoring Container Health")
+
+@app.get("/")
+async def root():
+    return listing_containers()
+
+
