7e0daf6eaecaae361a3e48dfd11f07023e06de0a
Georges Delrieu <georges.delrieu@sciencespo.fr>
2022-01-08 09:28:57 +0100

    adding docker compose

--- status-state-api/app/main.py
+++ status-state-api/app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from apscheduler.schedulers.background import BackgroundScheduler
 import docker
-import logging
-
 
 app = FastAPI(title="Status State API",description="Recurringly checks the status of locally running containers")
-logger = logging.getLogger(__name__)
 
 def listing_containers():
     client = docker.from_env()
@@ -16,14 +12,9 @@ def listing_containers():
             "container ID": str(running_container.id)} 
             for running_container in running_containers]
 
-
-scheduler = BackgroundScheduler(daemon = True)
-scheduler.add_job(listing_containers, 'interval', seconds=30)
-
 @app.on_event("startup")
 def display_statuses():
-    scheduler.start()
-    logger.info("Monitoring Container Health")
+    listing_containers()
 
 @app.get("/")
 async def root():
