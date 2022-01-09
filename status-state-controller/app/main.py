e81e4c32b65b989d0d335be49f7d0c69a670326c
Georges Delrieu <georges.delrieu@sciencespo.fr>
2022-01-09 13:20:45 +0100

    replacing fastapi with asyncio

--- status-state-controller/app/main.py
+++ status-state-controller/app/main.py
@@ -1,42 +1,41 @@
-import requests
+from func import fetching_containers, clearing_containers, restarting_containers
+import asyncio
 import docker
-from fastapi import FastAPI
-from apscheduler.schedulers.background import BackgroundScheduler
-import ast
-import logging
 
-# Initializing the app and the logging
-app = FastAPI(title="Status State Controller",description="Keeps required containers up and shuts down other containers")
-logging.basicConfig(level=logging.INFO)
 
-def fetching_containers():
-    r = requests.get('http://host.docker.internal:80')
-    return {container["container ID"]: container["name"] for container in ast.literal_eval(r.text)}
+loop = None
 
-def clearing_containers(status_state_list):
-    '''Destroys containers outside the list of required containers'''
+async def asgi_handler(scope, receiver, sender):
+  global loop
+  if scope['type'] == 'lifespan':
+    message = await receiver()
+    if message['type'] == 'lifespan.startup':
+      loop = asyncio.get_running_loop()
+      loop.create_task(cron())
+      await sender({'type': 'lifespan.startup.complete'})
+    elif message['type'] == 'lifespan.shutdown':
+      await sender({'type': 'lifespan.shutdown.complete'})
+    return
+
+  await sender({'type': 'http.response.start', 'status': 200, 'headers': [[b'content-type', b'text-plain']] })
+  await sender({'type': 'http.response.body', 'body': b'fine test'})
+
+
+async def cron():
     client = docker.from_env()
-    logging.info('Checking for unwelcome containers')
-    for running_container in client.containers.list():
-        if running_container.id not in status_state_list:
-            logging.warning(f'Container {running_container.name} should not be up... shutting it down')
-            running_container.stop()
-
-def restarting_containers(status_state_list):
-    '''Restarts stopped containers among the required containers'''
-    client = docker.from_env()
-    id_running_containers = [container.id for container in client.containers.list()]
-    logging.info('Checking for missing containers')
-    for required_container in status_state_list.keys():
-        if required_container not in id_running_containers:
-            logging.warning(f'Container {status_state_list[required_container]} should not be down... restarting it')
-            container = client.containers.get(required_container)
-            container.start()
-
-@app.on_event("startup")
-def launch():
     status_state_list = fetching_containers()
-    scheduler = BackgroundScheduler(daemon = True)
-    scheduler.add_job(clearing_containers, 'interval', seconds=30, args=[status_state_list])
-    scheduler.add_job(restarting_containers, 'interval', seconds=30, args=[status_state_list])
-    scheduler.start()
+    while True:
+        clearing_containers(status_state_list, client)
+        restarting_containers(status_state_list, client)
+        await asyncio.sleep(30)
+
+if __name__ == '__main__':
+  import uvicorn
+  uvicorn.run("main:asgi_handler",
+                host = '0.0.0.0',
+                port = 8000,
+                lifespan = 'on',
+                workers = 1,
+                log_level = "warning",
+                interface = 'asgi3',
+                timeout_keep_alive = 5)
