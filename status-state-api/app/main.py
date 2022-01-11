cedaaf182b500b172a642240e4eed4c3e4e1f95e
Georges Delrieu <georges.delrieu@sciencespo.fr>
2022-01-11 13:48:31 +0100

    cleanup api

--- status-state-api/app/main.py
+++ status-state-api/app/main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
-import docker
+from app.utils import getting_container_list
 
-app = FastAPI(title="Status State API",description="Recurringly checks the status of locally running containers")
+app = FastAPI(title="Status State API",description="Checks the status of locally running containers at launch")
 
 def listing_containers():
-    client = docker.from_env()
-    running_containers = client.containers.list()
-    return [{"name": str(running_container.name),
-            "status": str(running_container.status),
-            "image": str(running_container.image),
-            "container ID": str(running_container.short_id)} 
-            for running_container in running_containers]
+    running_containers = getting_container_list()
+    try:
+        return [{"name": str(running_container.name),
+                "status": str(running_container.status),
+                "image": str(running_container.image),
+                "container ID": str(running_container.short_id)} 
+                for running_container in running_containers]
+    except AssertionError as e:
+        print(e)
+        print(f"Running containers format error. Your containers: {running_containers}")
+
 
 @app.on_event("startup")
 def display_statuses():
     listing_containers()
 
-@app.get("/")
-async def root():
+@app.get("/status")
+async def status():
     return listing_containers()
 
 
